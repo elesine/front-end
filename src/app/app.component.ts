77239943f8cb83df5864d04339767e27f616457b
Gretel Danitza Canasa Condori <gcanasac@bsg.com>
2022-11-30 19:16:43 -0500

    basic examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'front-end';
+  peliculas: Array<any>;
+
+  ngOnInit(): void {
+
+    setTimeout( ()=> {
+      // this.peliculas = [{
+      //   titulo: 'spider-man',
+      //   fechaLanzamiento: new Date(),
+      //   precio: 1400
+      // },{
+      //   titulo: 'Moana',
+      //   fechaLanzamiento: new Date('2016-11-14'),
+      //   precio: 300.99
+      // }]
+      this.peliculas = [];
+    },3000);
+
+  }
 }
