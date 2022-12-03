b24e77fa5495a2f249ddfbef35d385e0db040513
Gretel Danitza Canasa Condori <gcanasac@bsg.com>
2022-12-02 19:33:55 -0500

    forms

--- src/app/generos/crear-genero/crear-genero.component.ts
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crear-genero',
   templateUrl: './crear-genero.component.html',
-  styleUrls: ['./crear-genero.component.css']
+  styleUrls: ['./crear-genero.component.css'],
 })
-export class CrearGeneroComponent implements OnInit{
+export class CrearGeneroComponent implements OnInit {
+  form: FormGroup;
 
-  constructor(private router: Router) {
-
-  }
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.form =  this.formBuilder.group({
+      nombre: ''
+    });
   }
 
   guardarCambios() {
     //...Guardar cambios
-    this.router.navigate(['/generos'])
+    this.router.navigate(['/generos']);
   }
 }
